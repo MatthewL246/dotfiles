e73f759695962507f5f493630beda6d020768e91
Matthew Lopez <73856503+MatthewL246@users.noreply.github.com>
2025-06-17 22:43:48 -0400

    Set up a systemd-based initramfs and TPM-based encrypted partition unlocking

--- private_dot_config/private_aconfmgr/12-secure-boot.sh
+++ private_dot_config/private_aconfmgr/12-secure-boot.sh
@@ -5,6 +5,8 @@ AddPackage systemd-ukify
 
 CopyFileTo /etc/mkinitcpio.d/linux.preset-uki-enabled /etc/mkinitcpio.d/linux.preset
 CopyFile /etc/cmdline.d/root.conf
+CopyFile /etc/crypttab.initramfs 600
+
 # Apparently necessary to do this even when a file is removed - seems like an aconfmgr bug
 SetFileProperty /boot/loader/entries/arch-fallback.conf mode ''
 SetFileProperty /boot/loader/entries/arch.conf mode ''
@@ -14,7 +16,7 @@ RemoveFile /boot/EFI/Linux
 RemoveFile /boot/EFI
 
 # Secure Boot signing
-# TODO: This is not declarative - enrolling the keys requires manual setup
+# TODO: This is not declarative - enrolling the keys and setting up TPM unlocking requires manual setup
 AddPackage sbctl
 
 CreateFile /var/lib/sbctl/bundles.json >/dev/null
