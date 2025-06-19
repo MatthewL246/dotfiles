4692c49b00fbbdcce3de83ef51aa2cc69ce10bc5
Matthew Lopez <73856503+MatthewL246@users.noreply.github.com>
2025-06-18 23:35:03 -0400

    Set up KDE Plasma and SDDM
    (finally)

---
+++ private_dot_config/private_aconfmgr/30-desktop.sh
@@ -0,0 +1,34 @@
+## Desktop environment
+
+# KDE Plasma
+AddPackage plasma-meta
+
+# Reference for recommended packages: https://community.kde.org/Distributions/Packaging_Recommendations
+
+# Choices made for providers during Plasma installation
+# Already required by other KDE packages
+AddPackage noto-fonts
+# Official and listed in the packaging recommendations
+AddPackage phonon-qt6-vlc
+# Pipewire is the modern sound protocol
+AddPackage pipewire-jack
+# Not sure that this one really matters, just using ffmpeg
+AddPackage qt6-multimedia-ffmpeg
+
+# Default service configuration
+CreateLink /etc/systemd/user/default.target.wants/xdg-user-dirs-update.service /usr/lib/systemd/user/xdg-user-dirs-update.service
+CreateLink /etc/systemd/user/pipewire-session-manager.service /usr/lib/systemd/user/wireplumber.service
+CreateLink /etc/systemd/user/pipewire.service.wants/wireplumber.service /usr/lib/systemd/user/wireplumber.service
+CreateLink /etc/systemd/user/sockets.target.wants/pipewire-pulse.socket /usr/lib/systemd/user/pipewire-pulse.socket
+CreateLink /etc/systemd/user/sockets.target.wants/pipewire.socket /usr/lib/systemd/user/pipewire.socket
+
+# Noto fonts default configuration
+CreateLink /etc/fonts/conf.d/46-noto-mono.conf /usr/share/fontconfig/conf.default/46-noto-mono.conf
+CreateLink /etc/fonts/conf.d/46-noto-sans.conf /usr/share/fontconfig/conf.default/46-noto-sans.conf
+CreateLink /etc/fonts/conf.d/46-noto-serif.conf /usr/share/fontconfig/conf.default/46-noto-serif.conf
+CreateLink /etc/fonts/conf.d/66-noto-mono.conf /usr/share/fontconfig/conf.default/66-noto-mono.conf
+CreateLink /etc/fonts/conf.d/66-noto-sans.conf /usr/share/fontconfig/conf.default/66-noto-sans.conf
+CreateLink /etc/fonts/conf.d/66-noto-serif.conf /usr/share/fontconfig/conf.default/66-noto-serif.conf
+
+# Enable SDDM start on boot
+CreateLink /etc/systemd/system/display-manager.service /usr/lib/systemd/system/sddm.service
