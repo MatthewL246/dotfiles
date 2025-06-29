bdd3790313ac17e200a79bef353d3c5e9a03d14c
Matthew Lopez <73856503+MatthewL246@users.noreply.github.com>
2025-06-28 22:40:16 -0400

    Add Piper application for G502 X mouse configuration

--- private_dot_config/private_aconfmgr/30-desktop.sh
+++ private_dot_config/private_aconfmgr/30-desktop.sh
@@ -65,3 +65,7 @@ CopyFile /var/lib/AccountsService/users/matthew
 # Applications useful for Chezmoi
 AddPackage kompare # 2-way diffs
 AddPackage kdiff3 # 3-way merges
+
+# G502 X mouse configuration
+AddPackage piper
+CreateLink /etc/systemd/system/dbus-org.freedesktop.ratbag1.service /usr/lib/systemd/system/ratbagd.service
